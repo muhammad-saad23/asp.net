a38422be1231e1244f538abacaaec6fd8e8945fb
saad <ms22458881@gmail.com>
2024-03-19 09:33:16 +0500

    added

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuctions
+{
+    internal class Employee
+    {
+        public int empid;
+        public string empname;
+        public string empemail;
+
+        public Employee()
+        {
+            Console.WriteLine("first constructor");
+        }
+
+        public Employee(int empid) : this()
+        {
+            this.empid = empid;
+            Console.WriteLine("Second constructor"+ empid);
+        }
+        public Employee(string empname, string empemail) : this(77)
+        {
+            this.empname = empname;
+            this.empemail = empemail;
+            Console.WriteLine($"third constructor emp id is {empid} and empname is:{empname} with email:{empemail}");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace fuctions
+{
+    internal class Program
+    {
+        //void msg()
+        //{
+        //    //Console.WriteLine("my name is saad");
+        //}
+        //void name(string myname)
+        //{
+        //    Console.WriteLine($"my name is {myname}");
+        //}
+        // static string msg1(string val1,string val2)
+        //{
+        //    return $"static fucntion returns{val1} and {val2}";
+
+        //}
+        //public class student
+        //{
+        //    public int stdid;
+        //    public string name;
+        //    public string gender;
+        //    public string @class;
+
+        //    public string details()
+        //    {
+        //        return $"stdid is {stdid} and name is {name} and gender is {gender}  and class is {@class}";
+        //    }
+        
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            ////Program obj=new Program();
+            //Program obj1 = new();
+
+            //obj1.msg();
+            //obj1.name("saad");
+            //string val = Program.msg1("1", "2");
+            //Console.WriteLine(val);
+
+            //student std = new();
+            //std.stdid = 1;
+            //std.name = "saad";
+            //std.gender = "male";
+            //std.@class = "masters";
+            //Console.WriteLine(std.details);
+
+            //Employee emp= new("saad","saad@gmail.com");
+
+            //Car parent = new Car();
+            //double_cabin child=new double_cabin("toyota rocco","black","toyota");
+            ////child.varient();
+            //child.canrun();
+
+            A parent new A();
+            C child=new B();
+            child.method3();
+        }
+
+      
+    }
+}
